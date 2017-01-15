990a67d647ec013e372b09381c7b2cf70cd86254
Kuzjma Korsakov <korsakov.kuzjma@gmail.com>
2017-01-16 01:25:19 +0800

    eee

--- wrap.go
+++ wrap.go
@@ -3,7 +3,6 @@ package wrap
 import (
 	"net/http"
 )
-
 func Wrap(fw ...func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		for _, ffw := range fw {
